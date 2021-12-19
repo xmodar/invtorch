68e0bff62d10dcfd8abf3e5942f6c1c6f0ac30f7
xmodar <modar.alfadly@gmail.com>
2021-12-19 04:07:22 +0300

    Fix passing :mode down in Sequential

--- invtorch/nn/modules/container.py
+++ invtorch/nn/modules/container.py
@@ -15,20 +15,28 @@ class Sequential(WrapperModule):
         super().__init__(nn.Sequential(*modules))
 
     def function(self, *args, **kwargs):
-        out_cache = kwargs.pop('cache', {})
-        out_cache['kwargs'] = []
+        cache = kwargs.pop('cache', None)
+        if cache is not None:
+            all_kwargs = []
+            cache['kwargs'], cache = all_kwargs, cache.copy()
+            clean_cache = cache
         for layer in self.module:
-            cache = {}
+            if cache is not None:
+                cache = clean_cache.copy()
             args = layer.call_function(*pack(args), **kwargs, cache=cache)
-            out_cache['kwargs'].append(cache)
+            if cache is not None:
+                cache.pop(':mode', None)
+                all_kwargs.append(cache)
             kwargs = {}
         return args
 
     def inverse(self, *args, kwargs=None, cache=None):
         # pylint: disable=arguments-differ, unused-argument
+        if cache is not None and cache.get(':saved'):
+            cache[':saved'] = set()
         for i, layer in enumerate(reversed(self.module), 1):
             rest = {} if kwargs is None else kwargs[-i]
-            args = layer.call_inverse(*pack(args), **rest)
+            args = layer.call_inverse(*pack(args), **rest, cache=cache)
         return args
 
     call_function, call_inverse = function, inverse
